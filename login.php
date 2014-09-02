719d4917d753a73746f29c31fa22def1d8e05faa
David Söderberg <granskog_@hotmail.com>
2014-09-02 10:26:55 +0200

    Fixat alla felmeddelanden, testning är nästa sak att göra.

--- login.php
+++ login.php
@@ -22,9 +22,12 @@ class Login{
 	private function hasCookie(){
 		if(isset($_COOKIE['login'])){
 			if($_COOKIE['login'] == $this->secureStorage($this->username.$this->password)){
+				$this->error = 'Inloggning lyckades via cookies';
 				return true;
 			}
 			unset($_COOKIE['login']);
+			setcookie('login', '', time() - 3600);
+			$this->error = 'Felaktig information i cookie';
 		}
 		return false;
 	}
@@ -51,26 +54,36 @@ class Login{
 		if(isset($_SESSION['login'])) {
 			unset($_SESSION['login']);
 			session_destroy();
+			$this->error = 'Du har nu loggat ut';
 		}
 		if(isset($_COOKIE['login'])) {
 			unset($_COOKIE['login']);
+			setcookie('login', '', time() - 3600);
 		}
 	}
 
 	private function isLoggedIn(){
 		if(!$this->hasCookie() && !$this->hasSession()){
-			if(isset($_POST['username']) && isset($_POST['password'])) {
+			if(isset($_POST['username']) && isset($_POST['password']) && $_POST['username'] != '' && $_POST['password'] != '') {
 				$this->inputUsername = $_POST['username'];
 				if($_POST['username'] == $this->username && $this->password == $_POST['password']) {
 					$this->setSession();
 					$this->isLoggedIn = true;
+					$this->error = 'Inloggning lyckades';
 					if (isset($_POST['cookie'])) {
 						$this->setCookie();
+						$this->error = 'Inloggning lyckades och vi kommer ihåg dig nästa gång';
 					}
 				}else{
-					$this->error = 'Ditt användarna och/eller lösenord är fel.';
+					$this->error = 'Felaktigt användarnamn och/eller lösenord';
 				}
 			}else{
+				if(isset($_POST['username']) && $_POST['username'] == '') {
+					$this->error = 'Användarnamn saknas';
+				}
+				if(isset($_POST['password']) && $_POST['password'] == '') {
+					$this->error = 'Lösenord saknas';
+				}
 
 			}
 		}else{
@@ -79,22 +92,28 @@ class Login{
 	}
 
 	public function renderHtml(){
+		echo '<h1>Laborationskod ds222hz</h1>';
 		if(!$this->isLoggedIn) {
 			?>
-				<h2>test</h2>
+				<h2>Ej Inloggad</h2>
 				<form action="./" METHOD="post">
-					<?php echo '<p>' . $this->error . '</p>'; ?>
-					<label for="username">Username:</label>
-					<input type="text" name="username" id="username" value="<?php echo $this->inputUsername; ?>"/>
-					<label for="password">Password:</label>
-					<input type="password" name="password" id="password"/>
-					<label for="cookie">Kom ihåg mig:</label>
-					<input type="checkbox" id="cookie" name="cookie" value="yes"/>
-					<input type="submit" value="Logga in"/>
+					<fieldset>
+						<legend>Login - skriv in användarnamn och lösenord</legend>
+						<?php echo '<p>' . $this->error . '</p>'; ?>
+						<label for="username">Användarnamn:</label>
+						<input type="text" name="username" id="username" value="<?php echo $this->inputUsername; ?>"/>
+						<label for="password">Lösenord:</label>
+						<input type="password" name="password" id="password"/>
+						<label for="cookie">Håll mig inloggad:</label>
+						<input type="checkbox" id="cookie" name="cookie" value="yes"/>
+						<input type="submit" value="Logga in"/>
+					</fieldset>
 				</form>
 			<?php
 		}else{
 			?>
+			<h2>Admin är inloggad</h2>
+			<p><?php echo $this->error; ?></p>
 				<a href="./?logout">Logga ut</a>
 			<?php
 		}
