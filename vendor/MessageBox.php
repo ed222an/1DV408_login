a7b5b2b2c205214f48ba373a69c1a5434eac1e52
David Söderberg <granskog_@hotmail.com>
2014-09-09 14:12:37 +0200

    Skapar allt innehåll i klassen MessageBox.

--- vendor/MessageBox.php
+++ vendor/MessageBox.php
@@ -2,14 +2,22 @@
 
 class MessageBox{
 
-	private $key = "";
+	private $key = "message";
 
-	public function set($string){
-
+	public function set($value){
+		setcookie( $this->key, $value, -1);
 	}
 
 	public function get(){
+		if (isset($_COOKIE[$this->key])) {
+			$message = $_COOKIE[$this->key];
+		}else {
+			$message = "";
+		}
 
+		setcookie($this->key, "", time() -1);
+
+		return $message;
 	}
 
 }
